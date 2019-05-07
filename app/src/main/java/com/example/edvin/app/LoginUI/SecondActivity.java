0172bcea21929829dcb7043edf5c4cff9f1be327
Sina <37536601+sina96@users.noreply.github.com>
2019-05-07 13:06:44 +0200

    UNDO CLASSES

--- app/src/main/java/com/example/edvin/app/LoginUI/SecondActivity.java
+++ app/src/main/java/com/example/edvin/app/LoginUI/SecondActivity.java
@@ -1,12 +1,38 @@
 package com.example.edvin.app.LoginUI;
 
-import android.os.Bundle;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.Login;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 import com.example.edvin.app.LoginData.LoginBackground;
 import com.example.edvin.app.R;
@@ -16,6 +42,18 @@ public class SecondActivity extends AppCompatActivity {
     EditText passTxt;
     EditText userTxt;
     Button startBtn;
+
+
+    CallbackManager callbackManager;
+    TextView txtEmail, txtBirthday, txtFriends;
+    ProgressDialog mDialog;
+    ImageView imgAvatar;
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +65,81 @@ public class SecondActivity extends AppCompatActivity {
         userTxt = findViewById(R.id.userTxt);
         startBtn = findViewById(R.id.startBtn);
 
+        callbackManager = CallbackManager.Factory.create();
+        txtBirthday = (TextView) findViewById(R.id.txtBirthday);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        txtFriends = (TextView) findViewById(R.id.txtFriends);
+
+        imgAvatar = (ImageView) findViewById(R.id.avatar);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                mDialog = new ProgressDialog(SecondActivity.this);
+                mDialog.setMessage("Retrieving data...");
+                mDialog.show();
+
+                String accesstoken = loginResult.getAccessToken().getToken();
+
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        mDialog.dismiss();
+                        Log.d("response", object.toString());
+                        getData(object);
+                    }
+                });
+
+                //Request Graph API
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,email,birthday,friends");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        //If already logged in
+        if (AccessToken.getCurrentAccessToken() != null) {
+            //Set the userID
+            txtEmail.setText(AccessToken.getCurrentAccessToken().getUserId());
+        }
+
     }
 
 
-    public void onClick(View view){
+    private void getData(JSONObject object) {
+        try {
+            URL profile_picture = new URL("https://graph.facebook.com/" + object.getString("id") + "/picture?width=250&height=250");
 
+            Picasso.with(this).load(profile_picture.toString()).into(imgAvatar);
+
+            txtEmail.setText(object.getString("Email"));
+            txtBirthday.setText(object.getString("Birthday"));
+            txtFriends.setText("Friends: " + object.getJSONObject("friends").getJSONObject("summary").getString("total_count"));
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void onClick(View view) {
 
 
         String type = "login";
@@ -40,11 +148,8 @@ public class SecondActivity extends AppCompatActivity {
         String password = passTxt.getText().toString();
 
 
-
-
         LoginBackground bgw = new LoginBackground(this);
-        bgw.execute(type,username,password);
-
+        bgw.execute(type, username, password);
 
 
     }
