f121202b5d66fdf28687e982bc6d4b3512f535ce
Sina <37536601+sina96@users.noreply.github.com>
2019-05-15 21:53:24 +0200

    Added guide classes and layouts / Done by Sebastian and Elin

---
+++ app/src/main/java/com/example/edvin/app/guide/GuideMainActivity.java
@@ -0,0 +1,214 @@
+package com.example.edvin.app.guide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.edvin.app.R;
+
+public class GuideMainActivity extends AppCompatActivity {
+
+
+    private Button metall;
+    private Button glas;
+    private Button tidningar;
+    private Button batterier;
+    private Button farligtavfall;
+    private Button glodlampor;
+    private Button forpackningar;
+    private Button matavfall;
+    private Button elektronik;
+    private Button medicin;
+    private Button plast;
+    private Button papper;
+    private Button tradgard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_main);
+
+        metall = (Button) findViewById(R.id.metall);
+        metall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMetallActivity();
+            }
+        });
+
+        glas = (Button) findViewById(R.id.glas);
+        glas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGlasActivity();
+            }
+        });
+
+        tidningar = (Button) findViewById(R.id.tidningar);
+        tidningar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTidningarActivity();
+            }
+        });
+
+        batterier = (Button) findViewById(R.id.batterier);
+        batterier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBatterierActivity();
+            }
+        });
+
+        farligtavfall = (Button) findViewById(R.id.farligt_Avfall);
+        farligtavfall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFarligtavfallActivity();
+            }
+        });
+
+        metall = (Button) findViewById(R.id.metall);
+        metall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMetallActivity();
+            }
+        });
+
+        glodlampor = (Button) findViewById(R.id.glodlampor);
+        glodlampor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGlodlamporlActivity();
+            }
+        });
+
+        forpackningar = (Button) findViewById(R.id.forpackningar);
+        forpackningar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openForpackningarActivity();
+            }
+        });
+
+        matavfall = (Button) findViewById(R.id.matavfall);
+        matavfall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMatActivity();
+            }
+        });
+
+        elektronik = (Button) findViewById(R.id.elektronik);
+        elektronik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openElektronikActivity();
+            }
+        });
+
+        medicin = (Button) findViewById(R.id.medicin);
+        medicin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMedicinActivity();
+            }
+        });
+
+        plast = (Button) findViewById(R.id.plast);
+        plast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPlastActivity();
+            }
+        });
+
+        papper = (Button) findViewById(R.id.papper);
+        papper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPapperActivity();
+            }
+        });
+
+        tradgard = (Button) findViewById(R.id.tradgardsavfall);
+        tradgard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTradgardActivity();
+            }
+        });
+
+
+
+    }
+    public void openMetallActivity(){
+        Intent intent = new Intent(this, MetalActivity.class);
+        startActivity(intent);
+    }
+
+    public void openGlasActivity(){
+        Intent intent = new Intent(this, GlasActivity.class);
+        startActivity(intent);
+    }
+
+    public void openTidningarActivity(){
+        Intent intent = new Intent(this, TidningarActivity.class);
+        startActivity(intent);
+    }
+
+    public void openBatterierActivity(){
+        Intent intent = new Intent(this, BatterierActivity.class);
+        startActivity(intent);
+    }
+
+    public void openFarligtavfallActivity(){
+        Intent intent = new Intent(this, FarligtavfallActivity.class);
+        startActivity(intent);
+    }
+
+    public void openGlodlamporlActivity(){
+        Intent intent = new Intent(this, GlodlamporActivity.class);
+        startActivity(intent);
+    }
+
+    public void openForpackningarActivity(){
+        Intent intent = new Intent(this, ForpackningarActivity.class);
+        startActivity(intent);
+    }
+
+    public void openMatActivity(){
+        Intent intent = new Intent(this, MatActivity.class);
+        startActivity(intent);
+    }
+
+    public void openElektronikActivity(){
+        Intent intent = new Intent(this, ElektronikActivity.class);
+        startActivity(intent);
+    }
+
+    public void openMedicinActivity(){
+        Intent intent = new Intent(this, MedicinActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPlastActivity(){
+        Intent intent = new Intent(this, PlastActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPapperActivity(){
+        Intent intent = new Intent(this, PapperActivity.class);
+        startActivity(intent);
+    }
+
+    public void openTradgardActivity(){
+        Intent intent = new Intent(this, TradgardActivity.class);
+        startActivity(intent);
+    }
+
+}
