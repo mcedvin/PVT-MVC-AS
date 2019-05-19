5f791a666ec1e5c794f7d913c1bb033be1b3a023
KristinDahlen <kristin@dahlens.se>
2019-05-19 13:45:14 +0200

    add ChallengesActivity with some functionality

---
+++ app/src/main/java/com/example/edvin/app/challenges/ChallengesActivity.java
@@ -0,0 +1,81 @@
+package com.example.edvin.app.challenges;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.example.edvin.app.R;
+import com.example.edvin.app.mainpage.HomePageDesign;
+
+public class ChallengesActivity extends AppCompatActivity {
+
+    ImageButton backButton;
+    Button reduceButton;
+    Button reuseButton;
+    Button recycleButton;
+    ImageButton challengeOfTheMonthButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenges);
+
+        backButton = findViewById(R.id.backButton);
+        reduceButton = findViewById(R.id.reduceButton);
+        reuseButton = findViewById(R.id.reuseButton);
+        recycleButton = findViewById(R.id.recycleButton);
+        challengeOfTheMonthButton = findViewById(R.id.challengeOfTheMonthButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ChallengesActivity.this, HomePageDesign.class);
+
+                startActivity(i);
+            }
+        });
+
+
+        CategoryListener categoryListener = new CategoryListener();
+
+        reduceButton.setOnClickListener(categoryListener);
+        reuseButton.setOnClickListener(categoryListener);
+        recycleButton.setOnClickListener(categoryListener);
+        reduceButton.performClick();
+
+    }
+
+
+    class CategoryListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if (v.equals(reduceButton)) {
+                reduceButton.setBackgroundColor(getResources().getColor(R.color.coralColor));
+
+                reuseButton.setBackgroundColor(getResources().getColor(R.color.lightGrey));
+                recycleButton.setBackgroundColor(getResources().getColor(R.color.lightGrey));
+
+            } else if (v.equals(reuseButton)) {
+                reuseButton.setBackgroundColor(getResources().getColor(R.color.coralColor));
+
+                reduceButton.setBackgroundColor(getResources().getColor(R.color.lightGrey));
+                recycleButton.setBackgroundColor(getResources().getColor(R.color.lightGrey));
+
+            } else {
+                recycleButton.setBackgroundColor(getResources().getColor(R.color.coralColor));
+
+                reduceButton.setBackgroundColor(getResources().getColor(R.color.lightGrey));
+                reuseButton.setBackgroundColor(getResources().getColor(R.color.lightGrey));
+
+            }
+
+        }
+
+    }
+}
+
--- app/src/main/java/com/example/edvin/app/logininterface/MainActivity.java
+++ app/src/main/java/com/example/edvin/app/logininterface/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.edvin.app.R;
+import com.example.edvin.app.challenges.ChallengesActivity;
+import com.example.edvin.app.mainpage.HomePageDesign;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView logga = findViewById(R.id.logga);
+        logga.setOnClickListener(l -> startActivity(new Intent(this, ChallengesActivity.class)));
     }
 
 
