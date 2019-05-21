b421b8da442f9bdf601f335d6ebb526d57fd0890
KristinDahlen <kristin@dahlens.se>
2019-05-21 12:39:34 +0200

    add backButton to categoryViews

--- app/src/main/java/com/example/edvin/app/challenges/AirActivity.java
+++ app/src/main/java/com/example/edvin/app/challenges/AirActivity.java
@@ -1,15 +1,23 @@
 package com.example.edvin.app.challenges;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.edvin.app.R;
 
 public class AirActivity extends AppCompatActivity {
 
+    ImageButton backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_air);
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(l -> startActivity(new Intent(this, ChallengesCategoryScreenActivity.class)));
     }
 }
--- app/src/main/java/com/example/edvin/app/challenges/NatureActivity.java
+++ app/src/main/java/com/example/edvin/app/challenges/NatureActivity.java
@@ -1,15 +1,22 @@
 package com.example.edvin.app.challenges;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import com.example.edvin.app.R;
 
 public class NatureActivity extends AppCompatActivity {
 
+    ImageButton backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nature);
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(l -> startActivity(new Intent(this, ChallengesCategoryScreenActivity.class)));
     }
 }
