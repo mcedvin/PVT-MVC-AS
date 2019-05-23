7981ad2aad5808c176b30e3e7ad8bbf5acc94c27
Joel Larsson <joellarsson534@gmail.com>
2019-05-23 20:25:39 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml

--- app/src/main/java/com/example/edvin/app/FinishedChallenge.java
+++ app/src/main/java/com/example/edvin/app/FinishedChallenge.java
@@ -1,17 +1,24 @@
 package com.example.edvin.app;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.facebook.CallbackManager;
+import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.model.SharePhoto;
 import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareButton;
+import com.facebook.share.widget.ShareDialog;
 
 public class FinishedChallenge extends AppCompatActivity {
 
     Bitmap image;
-
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +35,24 @@ public class FinishedChallenge extends AppCompatActivity {
         ShareButton shareButton = (ShareButton)findViewById(R.id.fb_share_button);
         shareButton.setShareContent(content);
 
+
+
+    }
+
+    public void share(View view){
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+        if (ShareDialog.canShow(ShareLinkContent.class)) {
+            ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                    .setContentUrl(Uri.parse("http://developers.facebook.com/android"))
+                    .build();
+            shareDialog.show(linkContent);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
