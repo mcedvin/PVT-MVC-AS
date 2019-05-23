695bed2a7264398711efa31aff888f8c78bb0fda
Sebastian <sebastian.atug@hotmail.com>
2019-05-23 10:40:09 +0200

    avklarade utmaningar view

---
+++ app/src/main/java/com/example/edvin/app/FinishedChallenge.java
@@ -0,0 +1,13 @@
+package com.example.edvin.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FinishedChallenge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finished_challenge);
+    }
+}
