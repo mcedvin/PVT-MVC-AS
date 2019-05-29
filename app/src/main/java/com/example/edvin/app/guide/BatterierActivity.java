c731075b0e882a9dd24e5c30da1852f294480fe8
KristinDahlen <kristin@dahlens.se>
2019-05-29 15:06:05 +0200

    Small changes

--- app/src/main/java/com/example/edvin/app/guide/BatterierActivity.java
+++ app/src/main/java/com/example/edvin/app/guide/BatterierActivity.java
@@ -1,15 +1,21 @@
 package com.example.edvin.app.guide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.edvin.app.R;
+import com.example.edvin.app.challenges.ChallengesCategoryScreenActivity;
 
 public class BatterierActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_batterier);
+
     }
 }
